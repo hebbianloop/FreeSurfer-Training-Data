04ccab58d729787451b9c8dee528231459ec11f4
Shady <seldamat@gmail.com>
2017-07-11 17:23:12 -0400

    updated up to topology fix.. major issues with dura mislabeled as white matter.. brainmask & wm.mgz must be edited before generating final surfaces

--- sub-05/scripts/recon-all.cmd
+++ sub-05/scripts/recon-all.cmd
@@ -118,3 +118,118 @@ talairach_avi log file is transforms/talairach_avi.log...
 \n cp ../surf/lh.inflated.nofix ../surf/lh.inflated \n
 #@# Fix Topology lh Fri Jul  7 09:55:15 EDT 2017
 \n mris_fix_topology -rusage /Volumes/CFMI-CFS/sync/ADS/data/mri/FreeSurfer-Training-Data/sub-05/touch/rusage.mris_fix_topology.lh.dat -mgz -sphere qsphere.nofix -ga -seed 1234 sub-05 lh \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 13:26:50 EDT 2017 
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 13:27:01 EDT 2017 
+#--------------------------------------------
+#@# Skull Stripping Mon Jul 10 13:27:01 EDT 2017
+\n mri_watershed -rusage /Volumes/CFMI-CFS/sync/ADS/data/mri/FreeSurfer-Training-Data/sub-05/touch/rusage.mri_watershed.dat -keep brainmask.auto.mgz brainmask.mgz brainmask.mgz -T1 -brain_atlas /Volumes/CFMI-CFS/opt/fs6/average/RB_all_withskull_2016-05-10.vc700.gca transforms/talairach_with_skull.lta T1.mgz brainmask.auto.mgz \n
+\nINFO: brainmask.mgz already exists!
+The new brainmask.auto.mgz will not be copied to brainmask.mgz.
+This is done to retain any edits made to brainmask.mgz.
+Add the -clean-bm flag to recon-all to overwrite brainmask.mgz.\n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 13:27:26 EDT 2017 
+#-------------------------------------
+#@# EM Registration Mon Jul 10 13:27:26 EDT 2017
+\n mri_em_register -rusage /Volumes/CFMI-CFS/sync/ADS/data/mri/FreeSurfer-Training-Data/sub-05/touch/rusage.mri_em_register.dat -uns 3 -mask brainmask.mgz nu.mgz /Volumes/CFMI-CFS/opt/fs6/average/RB_all_2016-05-10.vc700.gca transforms/talairach.lta \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 13:35:18 EDT 2017 
+#--------------------------------------
+#@# CA Normalize Mon Jul 10 13:35:18 EDT 2017
+\n mri_ca_normalize -c ctrl_pts.mgz -mask brainmask.mgz nu.mgz /Volumes/CFMI-CFS/opt/fs6/average/RB_all_2016-05-10.vc700.gca transforms/talairach.lta norm.mgz \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 13:36:47 EDT 2017 
+#--------------------------------------
+#@# CA Reg Mon Jul 10 13:36:47 EDT 2017
+\n mri_ca_register -rusage /Volumes/CFMI-CFS/sync/ADS/data/mri/FreeSurfer-Training-Data/sub-05/touch/rusage.mri_ca_register.dat -nobigventricles -T transforms/talairach.lta -align-after -mask brainmask.mgz norm.mgz /Volumes/CFMI-CFS/opt/fs6/average/RB_all_2016-05-10.vc700.gca transforms/talairach.m3z \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 15:12:57 EDT 2017 
+#--------------------------------------
+#@# SubCort Seg Mon Jul 10 15:12:57 EDT 2017
+\n mri_seg_diff --seg1 aseg.auto.mgz --seg2 aseg.presurf.mgz --diff aseg.manedit.mgz \n
+\n mri_ca_label -relabel_unlikely 9 .3 -prior 0.5 -align norm.mgz transforms/talairach.m3z /Volumes/CFMI-CFS/opt/fs6/average/RB_all_2016-05-10.vc700.gca aseg.auto_noCCseg.mgz \n
+\n mri_cc -aseg aseg.auto_noCCseg.mgz -o aseg.auto.mgz -lta /Volumes/CFMI-CFS/sync/ADS/data/mri/FreeSurfer-Training-Data/sub-05/mri/transforms/cc_up.lta sub-05 \n
+#--------------------------------------
+#@# Merge ASeg Mon Jul 10 16:09:56 EDT 2017
+\n cp aseg.auto.mgz aseg.presurf.mgz \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:09:57 EDT 2017 
+#--------------------------------------------
+#@# Intensity Normalization2 Mon Jul 10 16:09:58 EDT 2017
+\n mri_normalize -mprage -aseg aseg.presurf.mgz -mask brainmask.mgz norm.mgz brain.mgz \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:12:49 EDT 2017 
+#--------------------------------------------
+#@# Mask BFS Mon Jul 10 16:12:49 EDT 2017
+\n mri_mask -T 5 brain.mgz brainmask.mgz brain.finalsurfs.mgz \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:12:51 EDT 2017 
+#--------------------------------------------
+#@# WM Segmentation Mon Jul 10 16:12:51 EDT 2017
+\n mri_binarize --i wm.mgz --min 255 --max 255 --o wm255.mgz --count wm255.txt \n
+\n mri_binarize --i wm.mgz --min 1 --max 1 --o wm1.mgz --count wm1.txt \n
+\n rm wm1.mgz wm255.mgz \n
+\n mri_segment -keep -mprage brain.mgz wm.seg.mgz \n
+\n mri_edit_wm_with_aseg -keep-in wm.seg.mgz brain.mgz aseg.presurf.mgz wm.asegedit.mgz \n
+\n mri_pretess -keep wm.asegedit.mgz wm norm.mgz wm.mgz \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:15:24 EDT 2017 
+#--------------------------------------------
+#@# Fill Mon Jul 10 16:15:24 EDT 2017
+\n mri_fill -a ../scripts/ponscc.cut.log -xform transforms/talairach.lta -segmentation aseg.auto_noCCseg.mgz wm.mgz filled.mgz \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:16:06 EDT 2017 
+#--------------------------------------------
+#@# Tessellate lh Mon Jul 10 16:16:06 EDT 2017
+\n mri_pretess ../mri/filled.mgz 255 ../mri/norm.mgz ../mri/filled-pretess255.mgz \n
+\n mri_tessellate ../mri/filled-pretess255.mgz 255 ../surf/lh.orig.nofix \n
+\n rm -f ../mri/filled-pretess255.mgz \n
+\n mris_extract_main_component ../surf/lh.orig.nofix ../surf/lh.orig.nofix \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:16:12 EDT 2017 
+#--------------------------------------------
+#@# Tessellate rh Mon Jul 10 16:16:12 EDT 2017
+\n mri_pretess ../mri/filled.mgz 127 ../mri/norm.mgz ../mri/filled-pretess127.mgz \n
+\n mri_tessellate ../mri/filled-pretess127.mgz 127 ../surf/rh.orig.nofix \n
+\n rm -f ../mri/filled-pretess127.mgz \n
+\n mris_extract_main_component ../surf/rh.orig.nofix ../surf/rh.orig.nofix \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:16:17 EDT 2017 
+#--------------------------------------------
+#@# Smooth1 lh Mon Jul 10 16:16:18 EDT 2017
+\n mris_smooth -nw -seed 1234 ../surf/lh.orig.nofix ../surf/lh.smoothwm.nofix \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:16:27 EDT 2017 
+#--------------------------------------------
+#@# Smooth1 rh Mon Jul 10 16:16:27 EDT 2017
+\n mris_smooth -nw -seed 1234 ../surf/rh.orig.nofix ../surf/rh.smoothwm.nofix \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:16:37 EDT 2017 
+#--------------------------------------------
+#@# Inflation1 lh Mon Jul 10 16:16:37 EDT 2017
+\n mris_inflate -no-save-sulc ../surf/lh.smoothwm.nofix ../surf/lh.inflated.nofix \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:16:55 EDT 2017 
+#--------------------------------------------
+#@# Inflation1 rh Mon Jul 10 16:16:55 EDT 2017
+\n mris_inflate -no-save-sulc ../surf/rh.smoothwm.nofix ../surf/rh.inflated.nofix \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:17:13 EDT 2017 
+#--------------------------------------------
+#@# QSphere lh Mon Jul 10 16:17:13 EDT 2017
+\n mris_sphere -q -seed 1234 ../surf/lh.inflated.nofix ../surf/lh.qsphere.nofix \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:19:04 EDT 2017 
+#--------------------------------------------
+#@# QSphere rh Mon Jul 10 16:19:05 EDT 2017
+\n mris_sphere -q -seed 1234 ../surf/rh.inflated.nofix ../surf/rh.qsphere.nofix \n
+\n\n#---------------------------------
+# New invocation of recon-all Mon Jul 10 16:20:55 EDT 2017 
+#--------------------------------------------
+#@# Fix Topology Copy lh Mon Jul 10 16:20:55 EDT 2017
+\n cp ../surf/lh.orig.nofix ../surf/lh.orig \n
+\n cp ../surf/lh.inflated.nofix ../surf/lh.inflated \n
+#@# Fix Topology lh Mon Jul 10 16:20:55 EDT 2017
+\n mris_fix_topology -rusage /Volumes/CFMI-CFS/sync/ADS/data/mri/FreeSurfer-Training-Data/sub-05/touch/rusage.mris_fix_topology.lh.dat -mgz -sphere qsphere.nofix -ga -seed 1234 sub-05 lh \n
